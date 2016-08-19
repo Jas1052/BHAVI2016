cfbbcbeb47d3d1253b6d070b7835445c916bebc0
Jas1052 <2017jliu@tjhsst.edu>
2016-08-18 23:03:17 -0400

    Webcrawler with Mongoose

--- Example_Applications/mongoose/node-mongoose-form/routes/form.js
+++ Example_Applications/mongoose/node-mongoose-form/routes/form.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Comment = mongoose.model('comments');
+var schemaName = mongoose.model('schemaName');
 var parseString = require('xml2js').parseString;
 var js2xmlparser = require("js2xmlparser");
 
 /* GET form. */
 router.get('/', function(req, res) {
-  Comment.find({}, '-_id jsonObject', function(err, comments){
+  schemaName.find({}, '-_id jsonObject', function(err, comments){
   	console.log(comments);
     console.log(JSON.stringify(comments));
     res.render(
@@ -22,7 +22,7 @@ router.post('/', function(req, res) {
 	var xml = "" + req.body.comment;
 	parseString(xml, function (err, result) {
 		//var obj = JSON.parse(result);
-	 	new Comment({jsonObject : result})
+	 	new schemaName({jsonObject : result})
 	 	.save(function(err, comment) {
 	    	//console.log(comment)
 	    	res.redirect('form');
---
+++ Example_Applications/mongoose/simple/server.js
@@ -0,0 +1,20 @@
+/*
+* More details here http://mongoosejs.com/docs/guide.html
+*/
+
+//require mongoose model defines in model/blog.js (see Files)
+var BlogPost = require("./model/blog");
+
+//create new model
+var post = new BlogPost({title: "My first post", author: "Yash Kumar", 
+													body: "We want to make documentation obsolete"});
+
+//save model to MongoDB
+post.save(function (err) {
+  if (err) {
+		return err;
+  }
+  else {
+  	console.log("Post saved");
+  }
+});
